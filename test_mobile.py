d6aa166d624e587b670075cf328f5885b4a3b95c
hasanulkabir-md <70855211+hasanulkabir-md@users.noreply.github.com>
2025-09-20 16:13:40 +0800

    Update test_mobile.py

--- test_mobile.py
+++ test_mobile.py
@@ -1,12 +1,13 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-def test_form_validation(browser):
-    browser.get("https://the-internet.herokuapp.com/forgot_password")
-    email_box = browser.find_element(By.ID, "email")
-    email_box.send_keys("test@example.com")
-    browser.find_element(By.ID, "form_submit").click()
+def test_form_validation():
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/forgot_password")
 
-    WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.TAG_NAME, "h1")))
-    assert "Internal Server Error" not in browser.page_source
+    driver.find_element(By.ID, "email").send_keys("test@example.com")
+    driver.find_element(By.ID, "form_submit").click()
+
+    assert "Internal Server Error" not in driver.page_source
+    driver.save_screenshot("screenshots/form_validation.png")
+    driver.quit()
